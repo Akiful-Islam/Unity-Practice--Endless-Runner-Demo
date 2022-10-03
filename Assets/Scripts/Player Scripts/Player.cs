b104b7afd8df6412f70b76894fdf90c37d35c87e
ScorpZer0 <arian.islam024@gmail.com>
2022-10-03 20:49:07 +0600

    Player and BG prefab

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f;
+    public float maxVelocity = 5f;
+
+    private Rigidbody2D myBody;
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        myBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MovePlayerKeyboard();
+    }
+
+    void MovePlayerKeyboard()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = speed;
+            }
+
+            Vector3 temp = transform.localScale;
+            temp.x = 1.3f;
+            transform.localScale = temp;
+
+            animator.SetBool("isWalking", true);
+        }
+        else if (horizontalInput < 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = -speed;
+            }
+
+            Vector3 temp = transform.localScale;
+            temp.x = -1.3f;
+            transform.localScale = temp;
+
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+
     }
 }
